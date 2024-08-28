18b46d049228595304421215ee9af2fb668a4ac6
Eliton Miguel Van Ryn <elitonvanryn@gmail.com>
2024-08-28 08:50:38 -0300

    first commit

---
+++ src/main/java/com/vrsistemas/hobbyapp/server/HobbyAppServerApplication.java
@@ -0,0 +1,13 @@
+package com.vrsistemas.hobbyapp.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbyAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobbyAppServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vrsistemas/hobbyapp/server/HobbyAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrsistemas.hobbyapp.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyAppServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
